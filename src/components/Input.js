70c97c63085e14bf8d3c229cc68b046497f9506f
cllcnkrt <cllcnkrt@outlook.com>
2021-08-18 13:31:18 +0300

    todoSlice done

--- src/components/Input.js
+++ src/components/Input.js
@@ -1,20 +1,34 @@
-import React from 'react'
-import { useState } from 'react'
-import './Input.css'
+import React from "react";
+import { useState } from "react";
+import "./Input.css";
+
+import useDispatch from "react-redux";
+import { saveTodo } from "../features/todoSlice";
+
 function Input() {
-   
-    const [input, setInput] = useState("")
-    const addTodo =()=>{
-        
-    }
+  const [input, setInput] = useState("");
+  const dispatch = useDispatch();
 
+  const addTodo = () => {
+      console.log(`Adding ${input}`)
 
-    return (
-        <div className="input">
-            <input type="text"  value={input} onChange={(e)=> setInput(e.target.value)}/>
-            <button onClick={addTodo}>Add</button>
-        </div>
-    )
+      dispatch(saveTodo({
+          item:input,
+          done:false,
+          id: Date.now()
+      }))
+  };
+
+  return (
+    <div className="input">
+      <input
+        type="text"
+        value={input}
+        onChange={(e) => setInput(e.target.value)}
+      />
+      <button onClick={addTodo}>Add</button>
+    </div>
+  );
 }
 
-export default Input
+export default Input;
