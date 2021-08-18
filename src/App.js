78d3f6791be5a5644079e6f7904d3b500db82283
cllcnkrt <cllcnkrt@outlook.com>
2021-08-18 13:16:35 +0300

    input component and input.css done

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import './App.css';
+import Input from './components/Input';
 
 function App() {
   return (
-    <div className="App">
-      
+    <div className="app">
+      {/* todo list */}
+      {/* done */}
+      {/* input */}
+      <Input/>
     </div>
   );
 }
